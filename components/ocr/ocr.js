fc1aa287e795c62a4cdc2b71c1793231250d10f3
fucccccccck <1770622191@qq.com>
2018-08-23 15:27:58 +0800

    Initial commit

--- components/ocr/ocr.js
+++ components/ocr/ocr.js
@@ -1,7 +1,11 @@
+const app = getApp();
+
 Component({
   data: {
     display: true,
     image_path: "",
+    image_width: "",
+    image_height: "",
     text_result: "",
     current_target_id: "",
     scroll_top: 0,
@@ -9,48 +13,64 @@ Component({
   },
 
   methods: {
-    touchStart: function (event) {
-      this.setData({
-        current_target_id: event.currentTarget.id
-      });
+    chooseImage: function (event) {
+      chooseImage(this);
+    },
 
-      console.log(event, this.data.current_target_id);
+    closeImage: function (event) {
+      closeImage(this);
+    },
+
+    getCurrentTargetId: function (event) {
+      getCurrentTargetId(event, this);
     },
 
     syncRoll: function (event) {
-      if (event.currentTarget.id === this.data.current_target_id) {
-        this.setData({
-          scroll_top: event.detail.scrollTop,
-          scroll_left: event.detail.scrollLeft
-        });
-
-        console.log(event.currentTarget);
-      }
-    },
-
-    photograph: function (event) {
-      const that = this;
-
-      wx.chooseImage({
-        count: 1,
-        success: function (result) {
-          that.setData({
-            display: false,
-            image_path: result.tempFilePaths
-          });
-        }
-      })
-    },
-
-    refreshImage: function (event) {
-
-    },
-
-    closeImage: function (event){
-      console.log(event);
-      this.setData({
-        display: true
-      });
+      syncRoll(event, this);
     }
   }
 })
+
+function chooseImage(that) {
+  wx.chooseImage({
+    count: 1,
+    success: function (result) {
+      wx.getImageInfo({
+        src: result.tempFilePaths[0],
+        success: function (result) {
+          that.setData({
+            display: false,
+            image_path: result.path,
+            image_width: result.width + "px",
+            image_height: result.height + "px"
+          })
+        }
+      })
+    }
+  })
+}
+
+function closeImage(that) {
+  that.setData({
+    display: true,
+  })
+}
+
+function getCurrentTargetId(event, that) {
+  that.setData({
+    current_target_id: event.currentTarget.id
+  })
+
+  console.log(event.currentTarget);;
+}
+
+function syncRoll(event, that) {
+  if (event.currentTarget.id === that.data.current_target_id) {
+    that.setData({
+      scroll_top: event.detail.scrollTop,
+      scroll_left: event.detail.scrollLeft
+    });
+
+    console.log(event);
+  }
+}
