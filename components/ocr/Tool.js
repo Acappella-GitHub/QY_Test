fe689b94df08aa3eb10d0cc4f213e85dc311f13c
fucccccccck <1770622191@qq.com>
2018-08-29 15:30:11 +0800

    Initial commit

--- components/ocr/Tool.js
+++ components/ocr/Tool.js
@@ -1,8 +1,32 @@
-function wxGetImageInfo(src) {
+function wxGetImageInfo(src, size) {
   wx.getImageInfo({
     src,
     success: function (result) {
+      if ((result.width > 2048 || result.width < 15) || (result.height > 2048 || result.height < 15)){
+        wx.showModal({
+          title: ' 图片尺寸不符合要求 ',
+          content: ' 图片的长和宽，不能小于15像素，或大于2048像素 ',
+          showCancel: false
+        })
+        return;
+      }
 
+      switch(result.type) {
+        case 'jpg':
+        case 'jpeg':
+        case 'png':
+        case 'bmp':
+          break;
+        default:
+          wx.showModal({
+            title: ' 图片格式不符合要求 ',
+            content: ' 图片的格式必须为 jpg、jpeg、png、bmp ',
+            showCancel: false
+          })
+          break;
+      }
+
+      console.log(result.type);
     }
   })
 }
@@ -10,10 +34,8 @@ function wxGetImageInfo(src) {
 function wxChooseImage() {
   wx.chooseImage({
     count: 1,
-    sizeType: 'compressed',
     success: function (result) {
-      // wxGetImageInfo(result.tempFiles[0].path);
-      console.log(result.tempFiles[0].size);
+      wxGetImageInfo(result.tempFiles[0].path, result.tempFiles[0].size);
     }
   })
 }
